01295fc01402c96083fa3ff9b244120d90118b46
Agoston Herczegh <82599483+AgostonHerczegh@users.noreply.github.com>
2022-11-23 18:37:34 +0100

    Webfejlesztés, Vadásztársaság, UML in progress.

---
+++ Teki/Teki/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food[] foods = new Food[15];
+            foods[0] = new Salad();
+            foods[1] = new Crab(6);
+            foods[2] = new Crab(3);
+            foods[3] = new Salad(); 
+            foods[4] = new Crab(25);
+            foods[5] = new Crab(1);
+            foods[6] = new Crab(10);
+            foods[7] = new Crab(10);
+            foods[8] = new Crab(10);
+            foods[9] = new Crab(10);
+            foods[10] = new Crab(10);
+            foods[11] = new Crab(10);
+            foods[12] = new Crab(10);
+            foods[13] = new Crab(10);
+            foods[14] = new Crab(10);
+            Turtle turtle = new Turtle();
+            for (int i = 0; i < foods.Length; i++)
+            {
+                turtle.Eat(foods[i].Calories());
+            }
+        }
+    }
+}
---
+++ Teki/Teki/Turtle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teki
+{
+    class Turtle : Food
+    {
+        public uint happiness= 0;
+        public int fedLevel = 0;
+        public void Eat(int eat)
+        {
+            fedLevel+=eat;
+            Console.WriteLine("Fed level is now {0}", fedLevel);
+            if (fedLevel>1000)
+            
+                Console.WriteLine("Turtle is full.");
+            
+            if (fedLevel<500)
+            {
+                Console.WriteLine("Eaten calories are less than 500, turtle is gaining happiness.");
+                happiness++;
+            }
+            else
+            {
+                Console.WriteLine("Eaten calories are above 500, turtle is losing happiness.");
+                happiness--;
+            }
+            Console.WriteLine("Turtle's happiness is " + happiness);
+            Console.WriteLine("");
+        }
+    
+    }
+}
