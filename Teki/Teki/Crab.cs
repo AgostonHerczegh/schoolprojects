abbe349e0090e18a2ac2fd6dd95b0265a0674eae
Agoston Herczegh <82599483+AgostonHerczegh@users.noreply.github.com>
2022-11-23 16:46:05 +0100

    Add files via upload

---
+++ Teki/Teki/Crab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teki
+{
+    class Crab : Food
+    {
+        public Crab(int size)
+        {
+            if (size > 10 || size<1)
+            {
+                Console.WriteLine("Crab size is incorrect.");
+            }
+            else
+            {
+                this.size = size;
+            }
+
+
+        }
+        int size = 1;
+        public override int Calories()
+        {
+            return size * 10;
+        }
+    }
+}
