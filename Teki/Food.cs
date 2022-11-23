e4685d4ffbfad699bdeedc2455054194d1f727da
Agoston Herczegh <82599483+AgostonHerczegh@users.noreply.github.com>
2022-11-23 16:43:31 +0100

    Add project files.

---
+++ Teki/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teki
+{
+    abstract class Food
+    {
+        public virtual int Calories()
+        {
+            return 0;
+        }
+    }
+}
