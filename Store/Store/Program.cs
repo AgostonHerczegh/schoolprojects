9f4fd66e5c8172ce5afad14b740ac254248173a6
Agoston Herczegh <82599483+AgostonHerczegh@users.noreply.github.com>
2022-11-23 18:41:22 +0100

    Updated

--- Store/Store/Program.cs
+++ Store/Store/Program.cs
@@ -9,7 +9,6 @@
             Furniture[0] = new Table(10);
             Furniture[1] = new Bed(200, 180);
             Furniture[2] = new Chair(4);
-
             for (int i = 0; i < Furniture.Length; i++)
             {
                 Furniture[i].Print();
